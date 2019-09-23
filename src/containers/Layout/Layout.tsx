fbb0ab5d7e860bb0587f4610e0fce7ae95ac147a
Martin Komitsky <m.komitsky@corp.mail.ru>
2019-09-23 07:47:14 +0300

    removed unused styles

--- src/containers/Layout/Layout.tsx
+++ src/containers/Layout/Layout.tsx
@@ -3,27 +3,12 @@ import * as s from './Layout.css';
 import * as T from './Layout.types';
 
 const Layout: React.FunctionComponent<T.ILayoutProps> = props => {
-	const { header, children, footer } = props;
-	const HeaderElement = (
-		<header>
-			<div className={s.header}>{header}</div>
-		</header>
-	);
-	const FooterElement = (
-		<footer>
-			<div className={s.footer}>{footer}</div>
-		</footer>
-	);
-
+	const { children } = props;
 	return (
 		<main className={s.root}>
-			{header && HeaderElement}
-
 			<section className={s.contentWrapper}>
 				<div className={s.content}>{children}</div>
 			</section>
-
-			{footer && FooterElement}
 		</main>
 	);
 };
--- src/containers/Layout/Layout.types.ts
+++ src/containers/Layout/Layout.types.ts
@@ -1,5 +1,3 @@
 export interface ILayoutProps {
-	header?: React.ReactChild;
 	children: React.ReactChild | React.ReactChild[];
-	footer?: React.ReactChild;
 }
