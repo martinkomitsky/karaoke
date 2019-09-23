cfcc0d6a0f0c2b00117898e04dbfb4eca4637995
Martin Komitsky <m.komitsky@corp.mail.ru>
2019-09-23 06:55:29 +0300

    removed trash

--- src/types.ts
+++ src/types.ts
@@ -1,42 +1,8 @@
-export { IAppContext, IAppState } from './containers/App';
-
-export interface IImageItem {
-	id: string;
-	title: string;
-	description: string;
-	img: string;
-	editable: boolean;
-	created: number;
-}
-
-export interface IOptionItem {
-	label: string;
-	value: number;
-	undeletable?: boolean;
-}
-
-export interface IListItem {
-	name: string;
-	id: number;
-}
-
-export interface IFileProps {
-	height: number;
-	src: string;
-	width: number;
-}
-
 export interface IIndexedObject {
 	[key: string]: any;
 }
 
-export type TAsyncFileProps = Promise<IFileProps>;
-
 export type TObjectEntry = [string, any];
-
-export type TAnyPromise = Promise<any>;
-
-export type TFetchCallback = () => TAnyPromise;
 
 export type valueOf<T> = T[keyof T];
 
